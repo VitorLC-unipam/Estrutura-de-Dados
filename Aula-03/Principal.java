643c3ec851b2756fd0d66306f58ff6dbb7134659
VitorLC-unipam <vitorlc@unipam.edu.br>
2025-08-20 23:30:54 +0000

    Aula 3

---
+++ Aula-03/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+    public static void main(String[] args) {
+        Lista<String> lista = new Lista<String>("Alunos");
+        
+        lista.imprimeLista();
+
+        lista.addInicio("Rafael");
+        lista.addInicio("Gabriel");
+
+        lista.imprimeLista();
+    }
+}
